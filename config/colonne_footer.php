588c864407153cbae83bffbf40aab8cea7b577f3
yuribertoli <yuribertoli@yahoo.it>
2022-03-28 17:31:37 +0200

    footer start

---
+++ config/colonne_footer.php
@@ -0,0 +1,130 @@
+<?php
+
+$colonne = [
+
+    [
+      "titolo" => "dc comics",
+      "links" => [
+        [
+        "url" => "#",
+        "nome" => "Characters"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Comics"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Movies"
+        ],
+        [
+        "url" => "#",
+        "nome" => "TV"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Games"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Videos"
+        ],
+        [
+        "url" => "#",
+        "nome" => "News"
+        ]
+      ]
+    ],
+
+    [
+      "titolo" => "dc",
+      "links" => [
+        [
+        "url" => "#",
+        "nome" => "Terms Of Use"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Privacy policy (New)"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Ad Choices"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Advertising"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Jobs"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Subscriptions"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Talent Workshops"
+        ],
+        [
+        "url" => "#",
+        "nome" => "CPSC Certificates"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Ratings"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Shop Help"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Contact Us"
+        ]
+      ]
+    ],
+
+    [
+      "titolo" => "sites",
+      "links" => [
+        [
+        "url" => "#",
+        "nome" => "DC"
+        ],
+        [
+        "url" => "#",
+        "nome" => "MAD Magazine"
+        ],
+        [
+        "url" => "#",
+        "nome" => "DC Kids"
+        ],
+        [
+        "url" => "#",
+        "nome" => "DC Universe"
+        ],
+        [
+        "url" => "#",
+        "nome" => "DC Power Visa"
+        ]
+      ]
+    ],
+
+    [
+      "titolo" => "shop",
+      "links" => [
+        [
+        "url" => "#",
+        "nome" => "Shop DC"
+        ],
+        [
+        "url" => "#",
+        "nome" => "Shop DC Collectibles"
+        ]
+      ]
+    ]
+];
+
+return $colonne;
